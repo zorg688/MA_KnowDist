395d0e9fcad6b2ba4820036f54468b4f8541cfcb
Daniel <danielpietschke@gmx.de>
2025-11-13 20:43:14 +0200

    added all files

---
+++ Data_to_be_saved/models/cos-e/teacher/opusTCv20210807+bt.transformer-big.mk
@@ -0,0 +1,21 @@
+# cat+oci+spa-eng training data bigger than 10000000
+GPUJOB_HPC_MEM = 16g
+GPUJOB_SUBMIT  = -gpu01
+SUBWORD_VOCAB_SIZE    = 32000
+DEVSIZE    = 5000
+TESTSIZE   = 10000
+DEVMINSIZE = 200
+SRCLANGS    = cat oci spa
+TRGLANGS    = eng
+SKIPLANGS   = 
+LANGPAIRSTR = cat+oci+spa-eng
+DATASET     = opusTCv20210807+bt
+TRAINSET    = Tatoeba-train-v2021-08-07
+DEVSET      = Tatoeba-dev-v2021-08-07
+TESTSET     = Tatoeba-test-v2021-08-07
+PRE         = simple
+SUBWORDS    = spm
+SHUFFLE_DATA      = 0
+FIT_DEVDATA_SIZE  = 1000
+MAX_OVER_SAMPLING = 50
+USE_REST_DEVDATA  = 0
